e92e3d18bf19475dacb57d7448b9a462c92cc880
Paul Everitt <pauleveritt@me.com>
2016-10-14 16:56:40 -0400

    Added the results of scaffold.

---
+++ README.txt
@@ -0,0 +1,14 @@
+todos README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $VENV/bin/pip install -e .
+
+- $VENV/bin/initialize_todos_db development.ini
+
+- $VENV/bin/pserve development.ini
+
--- setup.py
+++ setup.py
@@ -1,17 +1,55 @@
-from setuptools import setup
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
 
 requires = [
     'pyramid',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
     'pyramid_tm',
     'SQLAlchemy',
     'transaction',
     'zope.sqlalchemy',
     'waitress',
-    'nose',
-    'webtest',
-    'psycopg2',
-    'sphinx'
-]
-setup(name='pyramid_sqltraversal',
-      install_requires=requires
+    ]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',  # includes virtualenv
+    'pytest-cov',
+    ]
+
+setup(name='todos',
+      version='0.0',
+      description='todos',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={
+          'testing': tests_require,
+      },
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = todos:main
+      [console_scripts]
+      initialize_todos_db = todos.scripts.initializedb:main
+      """,
       )
